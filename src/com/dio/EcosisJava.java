5d64e2e7d78e985a0b4cf16bee837539f7e8ad84
valterchess <valterrekabano7@gmail.com>
2022-01-28 20:13:53 -0300

    introdução ao ecosistema java

---
+++ src/com/dio/EcosisJava.java
@@ -0,0 +1,9 @@
+package com.dio;
+import com.dio .base.Order;
+
+public class EcosisJava {
+    public static void main(String[] args) {
+        Order order = new Order("code1234");
+        System.out.println(order);
+    }
+}
