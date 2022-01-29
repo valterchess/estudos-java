1316d320a441fba3c6e255938e7cf9b87ff5e9c4
valterchess <valterrekabano7@gmail.com>
2022-01-28 21:01:36 -0300

    estudos java | instro ecosistema e doc

--- src/com/dio/EcosisJava.java
+++ src/com/dio/EcosisJava.java
@@ -1,9 +1,11 @@
 package com.dio;
 import com.dio .base.Order;
 
+import java.math.BigDecimal;
+
 public class EcosisJava {
     public static void main(String[] args) {
-        Order order = new Order("code1234");
+        Order order = new Order("code1234", new BigDecimal("99.99") );
         System.out.println(order);
     }
 }
--- src/com/dio/base/Order.java
+++ src/com/dio/base/Order.java
@@ -1,16 +1,48 @@
 package com.dio.base;
 
+import java.math.BigDecimal;
+
+/**
+ * @author Valter Silva
+ * @version 1.0.0
+ * @see BigDecimal
+ * @since Release 1.0.0
+ */
 public class Order {
     private final String code;
+    private final BigDecimal totalValor;
+//private String[] itens;
 
-    public Order(String code) {
+    /**
+     * Construtor da Classe
+      * @param code Código do pedido
+     * @param totalValor Valor total do pedido
+     */
+    public Order(String code, BigDecimal totalValor) {
         this.code = code;
+        this.totalValor = totalValor;
     }
 
-    @Override
-    public String toString() {
-        return "Order{" +
-                "code='" + code + '\'' +
-                '}';
-    }
+    /**
+     * Calcula as taxas de acordo com o valor total do pedido.
+     * Se o valor for maior que R$100,00 uma taxa será cobrada.
+     * @return Valor total do pedido com taxas
+     * @throws RuntimeException se o valor do pedido for negativo
+     */
+    public BigDecimal calculaFee() throws RuntimeException {
+        if (this.totalValor.signum() < 0){
+            throw new RuntimeException("O pedido nao pode ter valor negativo.");
+        }
+    return (this.totalValor.compareTo(new BigDecimal("100.0")) > 100)
+            ? this.totalValor.multiply(new BigDecimal("0.99"))
+            : this.totalValor;
+}
+
+//    public void imprimirItens() {
+//        int i = 0;
+//        while (i < itens.length){
+//            System.out.println(itens[i]);
+//        }
+//    }
+
 }
