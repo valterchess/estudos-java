43229ddb7f2c069fefd55d98744be578ae0350fa
valterchess <valterrekabano7@gmail.com>
2022-01-31 19:59:36 -0300

    sintaxe variáveis e fluxo

---
+++ sintaxe-variaveis-e-fluxo/src/Conversao.java
@@ -0,0 +1,21 @@
+
+public class Conversao {
+
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		
+		float pontoFlutuante = 3.14f;
+		
+		int valor = (int)salario;
+		System.out.println(valor);	
+		
+		long numeroGrande = 323232323232l;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		
+		System.out.println(total);
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxo/src/Fatorial.java
@@ -0,0 +1,14 @@
+
+public class Fatorial {
+
+	public static void main(String[] args) {
+		int fatorial = 1;
+	    
+		for(int i = 1; i < 10; i++) {
+			fatorial *= i;
+			System.out.println("fatorial de " + i + " = " + fatorial);
+		}
+
+	}
+
+}
