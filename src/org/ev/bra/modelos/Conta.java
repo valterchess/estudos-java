3cfb600a08662ca81df4059ab845d3fdd5de5bcb
valterchess <valterrekabano7@gmail.com>
2022-01-31 01:27:50 -0300

    att metodos simples

--- src/org/ev/bra/modelos/Conta.java
+++ src/org/ev/bra/modelos/Conta.java
@@ -19,4 +19,26 @@ public class Conta {
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
+    public boolean saca(double valor){
+        if (this.saldo > 0 && valor <= this.saldo) {
+            this.saldo -= valor;
+            return true;
+        }
+            System.err.println("saldo insuficiente!");
+            return false;
+    }
+    public boolean deposita(double valor){
+        if (valor > 5) {
+            this.saldo += valor;
+            System.out.println("valor depositado com sucesso!");
+            return true;
+        }
+        System.out.println("valor mínimo para depósito é de R$5,00.");
+        return false;
+    }
+    public void transfere(Conta conta, double valor){
+        if (saca(valor)){
+            conta.deposita(valor);
+        }
+    }
 }
